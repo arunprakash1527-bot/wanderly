6e245d92cd0821567fcbaa5a87e622946972a3ef
Arun prakash <arunprakash1527@gmail.com>
2026-03-17 21:47:54 +0000

    Add missing public/index.html and src entry point

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WanderlyApp from './wanderly-app';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <WanderlyApp />
+  </React.StrictMode>
+);
